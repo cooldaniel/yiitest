3e62795cb094d1fe8c5ed27e2da9d4e1d7d2f870
cooldaniel <295313207@qq.com>
2018-05-14 11:52:48 +0800

    Fixed: protected/yiic.php 应该引用 framework/yiic.php 而不是 yii.php. 之前修改路径的时候改错了: https://github.com/cooldaniel/yiitest/commit/c055eee96a0834ce4d1a24a26ac3e482a0f913a2#diff-cd7b35a5f04dea81ea9a46156f445567

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,9 +1,11 @@
 <?php
 
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../framework/yii.php';
-$config=dirname(__FILE__).'/config/console.php';
 require_once(dirname(__FILE__).'/../../../projects/D/autoload.php');
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
 require_once(dirname(__FILE__).'/config/inc_constant.php');
 
 require_once($yiic);
