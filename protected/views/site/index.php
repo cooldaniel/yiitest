5ad4b5c24e32a02ad62463f2d9b24f8650021be1
cooldaniel <295313207@qq.com>
2019-12-13 13:46:23 +0800

    * 禁用首页的选择列表.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -8,11 +8,6 @@ $this->pageTitle=Yii::app()->name;
 <br/>
 <h3>接口列表</h3>
 
-<p class="hint">
-自动根据controller目录文件的action方法生成接口列表，勾选后就可以显示在站点首页接口列表.<br/>
-操作方式：勾选要显示的接口列表，去掉不需要显示的接口列表选中状态. <a href="<?php echo $this->createUrl('site/index'); ?>">index</a> <a href="<?php echo $this->createUrl('site/select'); ?>">select</a>
-</p>
-
 <form action="" method="post">
 
 <?php if ($showForm): ?>
@@ -32,7 +27,6 @@ $this->pageTitle=Yii::app()->name;
                 <?php foreach ($group as $row): ?>
                 <div>
                     <span class="span-6"><a href="<?php echo $row['url']?>"><?php echo $row['text']?></a></span>
-                    <span><input type="checkbox" name="form[checked_list][]" value="<?php echo $row['route']?>" <?php if ($row['checked']): ?>checked="checked"<?php endif; ?> /></span>
                 </div>
                 <?php endforeach; ?>
             </td>
